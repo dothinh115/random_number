e136094bdb4fb2973728d2fc4cc201f4c72874d5
dothinh115 <dothinh115@gmail.com>
2022-12-26 17:34:07 +0700

    update

--- index.js
+++ index.js
@@ -1,21 +1,62 @@
+let arr = [];
+const render = () => {
+    const arrShow = document.querySelector(".arrShow");
+    arrShow.innerHTML = "";
+    if(arr.length === 0) {
+        arrShow.innerText = "Chưa có gì trong mảng.";
+    } else {
+        arrShow.append("Số đã add: ");
+        for (let key in arr) {
+            arrShow.append(createBadge(arr[key], key));
+        }
+    }
+}
+
+const createBadge = (inner, index) => {
+    var badge = document.createElement("span");
+    var icon = document.createElement("i");
+    icon.classList = "fa-solid fa-xmark ml-2 icon";
+    icon.addEventListener("click", () => {
+        removeElement(index);
+    });
+    badge.classList = "badge badge-info mr-2";
+    badge.append(inner);
+    badge.append(icon);
+    return badge;
+}
+
+const removeElement = index => {
+    arr.splice(index, 1);
+    render();
+}
+
 submitHandle = () => {
     const value = document.querySelector("input").value;
-    let arr = [];
     const reg = /^[0-9]+$/;
     if (value === "") {
         alert("Nhập số vào!");
     } else {
-        arr = value.trim().split(",");
-        arr.forEach(item => {
-            if (item.trim().match(reg)) {
-                const random = randomElement(arr);
-                document.querySelector(".result b").innerHTML = random;
-            }
-            else {
-                alert("chỉ được nhập số");
-            }
-        });
+       if(value.trim().match(reg)) {
+        const find = arr.find(item => item === value.trim());
+        if(find) {
+            alert("Số đã có trong mảng!");
+        }
+        else {
+            arr.push(value.trim());
+        }
+        document.querySelector("input").value = "";
+        render();
+       }
+    }
+}
 
+const showResult = () => {
+    const result = document.querySelector(".result b");
+    if(arr.length === 0) {
+        alert("Nhập số vào mảng đã!");
+    }
+    else {
+        result.innerText = randomElement(arr);
     }
 }
 
@@ -29,3 +70,5 @@ document.querySelector("form").addEventListener("submit", (event) => {
     submitHandle();
     event.preventDefault();
 });
+
+render();
