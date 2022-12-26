a890cfbbb72597393e57514ecd947f68f087c6ef
dothinh115 <dothinh115@gmail.com>
2022-12-27 02:19:41 +0700

    little update

---
+++ class.js
@@ -0,0 +1,48 @@
+export class RandomNumber {
+    arr = [];
+
+    addNumber = (number) => this.arr.push(Number(number));
+
+    render = () => {
+        if (this.arr.length === 0) return "Chưa có gì trong mảng";
+        const renderHTML = this.arr.reduce((html, item, index) => {
+            return html += `
+                <span class="badge badge-info mr-2 px-2 py-1">
+                    ${item}
+                    <i class="fa-solid fa-xmark ml-2 icon" onclick="removeElement(${index})"></i>
+                </span>
+            `;
+        }, "");
+        return "Số đã add: " + renderHTML;
+    }
+
+    removeNumber = index => this.arr.splice(index, 1);
+
+    randomNumber = () => {
+        const number = this.arr.length;
+        const randomNumber = Math.floor(Math.random() * number);
+        return this.arr[randomNumber];
+    }
+}
+
+export class Validation {
+    message = "";
+
+    checkIfNumber = value => {
+        const reg = /^[0-9]+$/;
+        this.message = "";
+        if (!value.trim().match(reg) & value.trim() !== "") {
+            this.message = "Chỉ được nhập số";
+        }
+        return this.message;
+    }
+
+    checkIfExist = (arr, value) => {
+        this.message = "";
+        const find = arr.find(item => item == value);
+        if (find) {
+            this.message = "Số đã tồn tại!";
+        }
+        return this.message;
+    }
+}
--- index.js
+++ index.js
@@ -1,70 +1,42 @@
-let arr = [];
-let err = "";
-const render = () => {
-    const arrShow = document.querySelector(".arrShow");
-    arrShow.innerHTML = "";
-    arrShow.innerText = arr.length === 0 ? "Chưa có gì trong mảng." : "Số đã add: ";
-    for (let key in arr) arrShow.append(createBadge(arr[key], key));
-}
+import { RandomNumber, Validation } from "./class.js";
+const rnumb = new RandomNumber;
+const error = new Validation;
+let arrShow = document.querySelector(".arrShow");
+let input = document.querySelector("input");
+let result = document.querySelector(".result b");
 
-const createBadge = (inner, index) => {
-    var badge = document.createElement("span");
-    var icon = document.createElement("i");
-    icon.classList = "fa-solid fa-xmark ml-2 icon";
-    icon.addEventListener("click", () => removeElement(index));
-    badge.classList = "badge badge-info mr-2 px-2 py-1";
-    badge.append(inner, icon);
-    return badge;
-}
+const renderHTML = () => arrShow.innerHTML = rnumb.render();
 
-const removeElement = index => {
-    arr.splice(index, 1);
-    render();
+window.removeElement = index => {
+    rnumb.removeNumber(index);
+    renderHTML();
 }
 
 const inputChangeHandle = (e) => {
-    if(e.key !== "Enter") err = "";
-    const value = e.target.value;
-    const reg = /^[0-9]+$/;
-    if (!value.trim().match(reg) && value !== "") return err = "Chỉ được nhập số!";
-    const find = arr.find(item => item === value.trim());
-    if (find) err = "Số đã có trong mảng!";
+    if(e.key === "Enter") return;
+    showError();
+    if(error.checkIfNumber(input.value)) return showError(error.message);
+    if(error.checkIfExist(rnumb.arr, input.value)) return showError(error.message);
 }
 
 const submitHandle = (e) => {
     e.preventDefault();
-    const input = document.querySelector("input");
-    if(input.value === "") err = "Nhập số vào đã!" ;
-    showError();
-    if (!err) {
-        arr.push(input.value);
+    if(input.value === "") return showError("Nhập số vào!");
+    if(!error.message) {
+        rnumb.addNumber(input.value);
         input.value = "";
-        err = "";
+        renderHTML();
     }
-    render();
 }
 
-const showError = () => {
+const showError = message => {
     const errorEl = document.querySelector(".invalid-feedback");
-    const inputEl = document.querySelector("input");
-    errorEl.innerText = err;
-    err ? inputEl.classList.add("is-invalid") : inputEl.classList.remove("is-invalid");
+    errorEl.innerText = message;
+    message ? input.classList.add("is-invalid") : input.classList.remove("is-invalid");
 }
 
-const showResult = () => {
-    const result = document.querySelector(".result b");
-    arr.length === 0 ? err = "Nhập số vào mảng đã!" : result.innerText = randomElement(arr);
-}
+window.showResult = () => rnumb.arr.length === 0 ? showError("Nhập số vào mảng đã!") : result.innerText = rnumb.randomNumber();
 
-const randomElement = arr => {
-    const number = arr.length;
-    const randomNumber = Math.floor(Math.random() * number);
-    return arr[randomNumber];
-}
-
-document.querySelector("form").addEventListener("keyup", (e) => {
-    inputChangeHandle(e);
-    showError();
-});
+document.querySelector("form").addEventListener("keyup", (e) => inputChangeHandle(e));
 document.querySelector("form").addEventListener("submit", (e) => submitHandle(e));
-render();
+renderHTML();
