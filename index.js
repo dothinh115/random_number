6b9c0e012779211f51ed2118cf849f0a8f56829c
dothinh115 <dothinh115@gmail.com>
2022-12-26 18:50:23 +0700

    update

--- index.js
+++ index.js
@@ -2,26 +2,18 @@ let arr = [];
 const render = () => {
     const arrShow = document.querySelector(".arrShow");
     arrShow.innerHTML = "";
-    if(arr.length === 0) {
-        arrShow.innerText = "Chưa có gì trong mảng.";
-    } else {
-        arrShow.append("Số đã add: ");
-        for (let key in arr) {
-            arrShow.append(createBadge(arr[key], key));
-        }
-    }
+    if (arr.length === 0) return arrShow.innerText = "Chưa có gì trong mảng.";
+    arrShow.append("Số đã add: ");
+    for (let key in arr) arrShow.append(createBadge(arr[key], key));
 }
 
 const createBadge = (inner, index) => {
     var badge = document.createElement("span");
     var icon = document.createElement("i");
     icon.classList = "fa-solid fa-xmark ml-2 icon";
-    icon.addEventListener("click", () => {
-        removeElement(index);
-    });
+    icon.addEventListener("click", () => removeElement(index));
     badge.classList = "badge badge-info mr-2 px-2 py-1";
-    badge.append(inner);
-    badge.append(icon);
+    badge.append(inner, icon);
     return badge;
 }
 
@@ -33,31 +25,17 @@ const removeElement = index => {
 submitHandle = () => {
     const value = document.querySelector("input").value;
     const reg = /^[0-9]+$/;
-    if (value === "") {
-        alert("Nhập số vào!");
-    } else {
-       if(value.trim().match(reg)) {
-        const find = arr.find(item => item === value.trim());
-        if(find) {
-            alert("Số đã có trong mảng!");
-        }
-        else {
-            arr.push(value.trim());
-        }
-        document.querySelector("input").value = "";
-        render();
-       }
-    }
+    if (value === "") return alert("Nhập số vào!");
+    if (!value.trim().match(reg)) return alert("Chỉ được nhập số!");
+    const find = arr.find(item => item === value.trim());
+    find ? alert("Số đã có trong mảng!") : arr.push(value.trim());
+    document.querySelector("input").value = "";
+    render();
 }
 
 const showResult = () => {
     const result = document.querySelector(".result b");
-    if(arr.length === 0) {
-        alert("Nhập số vào mảng đã!");
-    }
-    else {
-        result.innerText = randomElement(arr);
-    }
+    arr.length === 0 ? alert("Nhập số vào mảng đã!") : result.innerText = randomElement(arr);
 }
 
 randomElement = arr => {
