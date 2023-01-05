7f89072774c6e02dc2fc10919408fcd716c34cd5
dothinh115 <dothinh115@gmail.com>
2023-01-05 14:58:53 +0700

    update

--- class.js
+++ class.js
@@ -1,39 +1,40 @@
 export class RandomNumber {
-    arr = [];
+  arr = [];
 
-    addNumber = (number) => this.arr.push(Number(number));
+  addNumber = (number) => this.arr.push(Number(number));
 
-    render = () => {
-        if (this.arr.length === 0) return "Chưa có gì trong mảng";
-        const renderHTML = this.arr.reduce((html, item, index) => {
-            return html += `
+  render = () => {
+    if (this.arr.length === 0) return "Chưa có gì trong mảng";
+    const renderHTML = this.arr.reduce((html, item, index) => {
+      return (html += `
                 <span class="badge badge-info mr-1 my-1 px-2 py-1">
                     ${item}
                     <i class="fa-solid fa-xmark icon ml-1" onclick="removeNumber(${index})"></i>
                 </span>
-            `;
-        }, "");
-        return "Số đã add: " + renderHTML;
-    }
+            `);
+    }, "");
+    return "Số đã add: " + renderHTML;
+  };
 
-    removeNumber = index => this.arr.splice(index, 1);
+  removeNumber = (index) => this.arr.splice(index, 1);
 
-    randomNumber = () => {
-        const number = this.arr.length;
-        const randomNumber = Math.floor(Math.random() * number);
-        return this.arr[randomNumber];
-    }
+  randomNumber = () => {
+    const number = this.arr.length;
+    const randomNumber = Math.floor(Math.random() * number);
+    return this.arr[randomNumber];
+  };
 }
 
 export class Validation {
-    message = "";
+  message = "";
 
-    validFunction (value, arr) {
-        const reg = /^[0-9]+$/;
-        const find = arr.find(item => item == value);
-        this.message = "";
-        if (!value.trim().match(reg) & value.trim() !== "") this.message = "Chỉ được nhập số";
-        if (find) this.message = "Số đã tồn tại!";
-        return this.message;
-    } 
+  validFunction(value, arr) {
+    const reg = /^[0-9]+$/;
+    const find = arr.find((item) => item == value);
+    this.message = "";
+    if (!value.trim().match(reg) && value.trim() !== "")
+      this.message = "Chỉ được nhập số";
+    if (find) this.message = "Số đã tồn tại!";
+    return this.message;
+  }
 }
